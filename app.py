c493dc30ee77e915ab9f6b6f4182c65a7612927a
GabyRkt <gabirakt@gmail.com>
2025-05-22 15:09:15 +0200

    Correct spells mistake

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from models import db, User
+from model import db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
