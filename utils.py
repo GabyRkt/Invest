16a9da381d98e6a29aac77b6880cd83b1a4bd711
GabyRkt <gabirakt@gmail.com>
2025-06-01 18:40:52 +0200

    Delete test files

--- utils.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-
-def get_etf_info(ticker_symbol):
-    try:
-        info = yf.Ticker(ticker_symbol).info
-        return {'fees': info.get('netExpenseRatio', 0.0), 'ticker': ticker_symbol}
-    except:
-        return {'fees': 0.0, 'ticker': ticker_symbol}
